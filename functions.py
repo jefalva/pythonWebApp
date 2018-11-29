f7c858fd572df2349964a89898e3b2af92da9cdc
alfredvergara <averg@hawaii.edu>
2018-11-29 03:45:20 +0000

    updated functions.py with hash function(incomplete)

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 import os
 import datetime
+import hashlib
 
+hasher = hashlib.md5()
+with open("verif.txt", "rb") as afile:
+	buf = afile.read()
+	hasher.update(buf)
+print(hasher.hexdigest())
 def verifysecret():
 	"This does secret things with a date and a key to verify identity"
 	time = datetime.date.today() #stores the day in the month it is
@@ -22,4 +28,13 @@ def verifyevent():
 		return 2 #code tos end if the last action was a transaction
 	else:
 		return 0 #code to send if last action does not exist
-print (verifyevent())
+def verifydb():
+	"This hashes and checks DB after each recorded event"
+	hasher = hashlib.md5()
+
+	fpath = os.path.join("home/linuxwebapp/sqlite3", "banker") #path to DB
+	with open(fpath, "rb") as afile:
+		buf = afile.read()
+		hasher.update(buf)
+	print(hasher.hexdigest())
+#print (verifyevent())
