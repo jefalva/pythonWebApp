11a6b947c718bd71a4b3bf92fee233c1674d2768
alfredvergara <averg@hawaii.edu>
2018-11-29 06:00:09 +0000

    updated functions.py to have working verifydb function

--- functions.py
+++ functions.py
@@ -10,7 +10,7 @@ print(hasher.hexdigest())
 def verifysecret():
 	"This does secret things with a date and a key to verify identity"
 	time = datetime.date.today() #stores the day in the month it is
-	fpath = os.path.join("/home/linuxwebapp/python3", "verif.txt") #path to secret
+	fpath = os.path.join("/home/server/python3", "verif.txt") #path to secret
 	with open (fpath, "r") as f: # reading file line by line into a list
 		content = f.readlines() #reads secret file into content
 		content = [x.strip() for x  in content] #trims newline
@@ -18,7 +18,7 @@ def verifysecret():
 
 def verifyevent():
 	"This logs verifies acceptable events based on the log"
-	fpath = os.path.join("/home/linuxwebapp/python3", "log.txt") #path to log
+	fpath = os.path.join("/home/server/python3", "log.txt") #path to log
 	with open (fpath, "r") as f: #reading file line by line into a list
 		content = f.readlines() #reads secret file into content
 		content = [x.strip() for x in content] # trims newline
@@ -29,12 +29,21 @@ def verifyevent():
 	else:
 		return 0 #code to send if last action does not exist
 def verifydb():
-	"This hashes and checks DB after each recorded event"
+	"This  verifies DB is legitimate with hashes"
+	golden = os.path.join("/home/server/python3", "dbverif.txt")
 	hasher = hashlib.md5()
-
-	fpath = os.path.join("home/linuxwebapp/sqlite3", "banker") #path to DB
+	fpath = os.path.join("/home/server/sqlite3", "banker")
 	with open(fpath, "rb") as afile:
 		buf = afile.read()
 		hasher.update(buf)
-	print(hasher.hexdigest())
-#print (verifyevent())
+	f = open(golden, "r")
+	output = f.read()
+	f.close()
+	if ((hasher.hexdigest() == output)):
+		f = open(golden, "w")
+		f.write(hasher.hexdigest())
+		f.close()
+		return True
+
+	else:
+		return False
